371291413aaa8f8edd8e7a06a4a644f2da76cc48
Arevik Azizyan <arevik.azizyan333@gmail.com>
2026-07-31 19:09:21 +0400

    Remove dist folder from git tracking

--- resolve-starter-nestjs/dist/common/ids.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.newId = newId;
-const crypto_1 = require("crypto");
-function newId(prefix) {
-    return `${prefix}_${(0, crypto_1.randomUUID)().slice(0, 8)}`;
-}
-//# sourceMappingURL=ids.js.map
